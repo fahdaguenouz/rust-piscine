6999e29c7376f7f7c9cb33cef03134c16fbd6684
fahdaguenouz <faguenouz@gmail.com>
2025-07-26 15:34:47 +0100

    simple hash

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    let mut res = HashMap::new();
+    for &word in words {
+     *res.entry(word).or_insert(0) +=1;
+        // if res.get(&word).is_some(){
+        //     res.1=res.1+1;
+        // }else{
+        //     res.insert(word,1)
+        // }
+    }
+    res
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    let mut  count=0;
+    frequency_count.len()
+    
+//     for word in frequency_count{
+     
+//             count +=1;
+        
+//     } 
+// count
+}

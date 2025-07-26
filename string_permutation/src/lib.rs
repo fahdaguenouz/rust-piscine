a239b0c3eb4938660876cecac65a1700e64aa84b
fahdaguenouz <faguenouz@gmail.com>
2025-07-26 16:03:41 +0100

    perm

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,18 +1,19 @@
  use std::collections::HashMap;
+
 pub fn is_permutation(s1: &str, s2: &str) -> bool {
-   
-    let mut w1=HashMap::new();
-    let mut w2=HashMap::new();
+    let mut w1 = HashMap::new();
+    let mut w2 = HashMap::new();
 
-    for n1 in s1.chars(){
-        // println!("{}",n1);
-        w1.insert(n1,1);
-    }
-    // println!("{:?}",w1);
-
-    for n2 in s2.chars(){
-        w2.insert(n2,1);
+    for c in s1.chars() {
+        *w1.entry(c).or_insert(0) += 1;
     }
 
-    w2==w1
+    for c in s2.chars() {
+        *w2.entry(c).or_insert(0) += 1;
+    }
+
+    // println!("{:?}", w1);
+    // println!("{:?}", w2);
+
+    w1 == w2
 }

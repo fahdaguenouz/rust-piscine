b138651e1aebf450c36b52f158e47078648b6f9e
fahdaguenouz <faguenouz@gmail.com>
2025-07-31 11:35:35 +0100

    unwrap or expect

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -7,30 +7,15 @@ pub enum Security {
 }
 impl Security {
     pub fn verify(&self, server: Result<&str, &str>) -> String {
-        
-        match self {
-            Security::Unknown => match server {
-                Ok(m) => m.to_string(),
-                Err(_) => panic!("UNKNOWN: server error"),
+     Security::Unknown=>server.unwrap(),
+      Security::Message => server.expect("ERROR: program stops"),
+    Security::Warning => server.unwrap_or("WARNING: check the server").to_string(),
+     Security::NotFound => server.unwrap_or_else(|e| panic!("Not found: {}", e)),
+    Security::UnexpectedUrl => match server {
+                Ok(m) => panic!("{}", m),
+                Err(e) => e.to_string(),
             },
-            Security::Message => match server {
-                Ok(m) => m.to_string(),
-                Err(_) => panic!("ERROR: program stops"),
-            },
-            Security::Warning => match server {
-                Ok(m) => m.to_string(),
-                Err(_) => panic!("WARNING: check the server"),
-            },
-            Security::NotFound => match server {
-                Ok(m) => m.to_string(),
-                Err(e) => panic!("Not found: {}", e),
-            },
-            Security::UnexpectedUrl => match server {
-                Ok(m) => m.to_string(),
-                Err(e) => panic!("Unexpected URL: {}", e),
-            },
-        }
-    }
+     
 }
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {

dc5c6d9d747b2e4133f969965282c8559112933f
fahdaguenouz <faguenouz@gmail.com>
2025-07-31 11:56:56 +0100

    unwrap or expect

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -7,15 +7,30 @@ pub enum Security {
 }
 impl Security {
     pub fn verify(&self, server: Result<&str, &str>) -> String {
-     Security::Unknown=>server.unwrap(),
-      Security::Message => server.expect("ERROR: program stops"),
-    Security::Warning => server.unwrap_or("WARNING: check the server").to_string(),
-     Security::NotFound => server.unwrap_or_else(|e| panic!("Not found: {}", e)),
-    Security::UnexpectedUrl => match server {
+        match self {
+            Security::Unknown => match server {
+                Ok(e) => e.to_string(),
+                Err(_) => server.unwrap().to_string(), 
+            },
+
+            Security::Message => match server {
+                Ok(n) => n.to_string(),
+                Err(_) => server.expect("ERROR: program stops").to_string(),
+            },
+
+            Security::Warning => server.unwrap_or("WARNING: check the server").to_string(),
+
+            Security::NotFound => match server {
+                Ok(v) => v.to_string(),
+                Err(e) => format!("Not found: {}", e),
+            },
+
+            Security::UnexpectedUrl => match server {
                 Ok(m) => panic!("{}", m),
                 Err(e) => e.to_string(),
             },
         }
+    }
 }
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {

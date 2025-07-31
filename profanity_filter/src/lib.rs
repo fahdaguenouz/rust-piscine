dd29497d18c3fe5c20d683c6f2c87fa4a6a49f0c
fahdaguenouz <faguenouz@gmail.com>
2025-07-31 13:18:29 +0100

    profanity

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,15 +1,10 @@
 
-#[derive(Debug)]
-pub enum MyResult<T, E> {
-    Ok(T),
-    Err(E),
-}
 
-pub fn check_ms(message: &str) -> MyResult<&str, &'static str> {
+pub fn check_ms(message: &str) -> Result<&str, &'static str> {
     let content = message.trim();
     if !content.is_empty() && !content.contains("stupid") {
-        MyResult::Ok(content)
+        Ok(content)
     } else {
-        MyResult::Err("ERROR: illegal")
+        Err("ERROR: illegal")
     }
 }

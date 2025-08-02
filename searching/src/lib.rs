2dd5ec370f12a915b8d119990488f481825c6bba
fahdaguenouz <faguenouz@gmail.com>
2025-08-02 11:44:26 +0100

    searching

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,12 +1,10 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-    let mut  index:Option<usize>=Some(0);
+    let mut  index:Option<usize>=None;
     for (i , nbr) in array.iter().enumerate(){
         if *nbr==key{
             index=Some(i);
         }
     }
-   if Some(0)==index{
-    return None;
-   }
+
    index
 }

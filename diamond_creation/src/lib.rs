9c07d03b55e8237cf515fe0cad6bed7a44dbc98d
fahdaguenouz <faguenouz@gmail.com>
2025-08-04 14:40:37 +0100

    diamand

--- diamond_creation/src/lib.rs
+++ diamond_creation/src/lib.rs
@@ -1,44 +1,57 @@
+
 pub fn get_diamond(c: char) -> Vec<String> {
     let mut result = Vec::new();
     
     let target = (c as u8 - b'A') as usize;
     
+    let total_width = if target == 0 { 1 } else { 2 * target + 1 };
+    
     for i in 0..=target {
         let letter = (b'A' + i as u8) as char;
-        let space = target - i;
+        let leading_spaces = target - i;
         
-        if i == 0 {
-            let line = format!("{}{}", " ".repeat(space), letter);
-            result.push(line);
+        let line = if i == 0 {
+            format!("{}{}{}", 
+                " ".repeat(leading_spaces), 
+                letter,
+                " ".repeat(leading_spaces)
+            )
         } else {
             let inner_spaces = 2 * i - 1;
-            let line = format!("{}{}{}{}", 
-                " ".repeat(space), 
+            let content = format!("{}{}{}{}", 
+                " ".repeat(leading_spaces), 
                 letter, 
                 " ".repeat(inner_spaces), 
                 letter
             );
-            result.push(line);
-        }
+            format!("{}{}", content, " ".repeat(leading_spaces))
+        };
+        
+        result.push(line);
     }
     
     for i in (0..target).rev() {
         let letter = (b'A' + i as u8) as char;
-        let space = target - i;
+        let leading_spaces = target - i;
         
-        if i == 0 {
-            let line = format!("{}{}", " ".repeat(space), letter);
-            result.push(line);
+        let line = if i == 0 {
+            format!("{}{}{}", 
+                " ".repeat(leading_spaces), 
+                letter,
+                " ".repeat(leading_spaces)
+            )
         } else {
             let inner_spaces = 2 * i - 1;
-            let line = format!("{}{}{}{}", 
-                " ".repeat(space), 
+            let content = format!("{}{}{}{}", 
+                " ".repeat(leading_spaces), 
                 letter, 
                 " ".repeat(inner_spaces), 
                 letter
             );
-            result.push(line);
-        }
+            format!("{}{}", content, " ".repeat(leading_spaces))
+        };
+        
+        result.push(line);
     }
     
     result

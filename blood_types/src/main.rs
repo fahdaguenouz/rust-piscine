52ee8af2c4e3254c9a62b8993ddabf98c8496369
fahdaguenouz <faguenouz@gmail.com>
2025-08-09 17:03:19 +0100

    blood

---
+++ blood_types/src/main.rs
@@ -0,0 +1,14 @@
+use blood_types::*;
+
+fn main() {
+	let blood_type: BloodType = "O+".parse().unwrap();
+	println!("{:?}",blood_type);
+	println!("recipients of O+ {:?}", blood_type.recipients());
+	println!("donors of O+ {:?}", blood_type.donors());
+	let another_blood_type: BloodType = "A-".parse().unwrap();
+	println!(
+		"donors of O+ can receive from {:?} {:?}",
+		&another_blood_type,
+		blood_type.can_receive_from(&another_blood_type)
+	);
+}

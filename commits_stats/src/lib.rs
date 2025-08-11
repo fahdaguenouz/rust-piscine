08bf01532c382e170b51636c32f77f5988f995d5
fahdaguenouz <faguenouz@gmail.com>
2025-08-11 14:52:51 +0100

    commits

--- commits_stats/src/lib.rs
+++ commits_stats/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+use json::JsonValue;
+use chrono::{DateTime, Utc, Datelike};
+
+pub fn commits_per_author(data: &JsonValue) -> HashMap<String, u32> {
+    let mut map = HashMap::new();
+    for commit in data.members() {
+        if let Some(login) = commit["author"]["login"].as_str() {
+            *map.entry(login.to_string()).or_insert(0) += 1;
+        }
+    }
+
+    map
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn commits_per_week(data: &JsonValue) -> HashMap<String, u32> {
+    let mut map = HashMap::new();
+    for commit in data.members() {
+        if let Some(date_str) = commit["commit"]["author"]["date"].as_str() {
+            if let Ok(date) = DateTime::parse_from_rfc3339(date_str) {
+                let date_utc: DateTime<Utc> = date.with_timezone(&Utc);
+                let iso_week = date_utc.iso_week();
+                let key = format!("{}-W{}", iso_week.year(), iso_week.week());
+                *map.entry(key).or_insert(0) += 1;
+            }
+        }
     }
+
+    map
 }

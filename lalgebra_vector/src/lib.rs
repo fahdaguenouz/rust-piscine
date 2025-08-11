c6db7f35cb6c40bb79069d6c8e9d027507de9255
fahdaguenouz <faguenouz@gmail.com>
2025-08-11 14:19:14 +0100

    vector

---
+++ lalgebra_vector/src/lib.rs
@@ -0,0 +1,38 @@
+use std::fmt::Debug;
+use std::ops::Add;
+use crate::Scalar;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Vector<T: Scalar>(pub Vec<T>);
+
+impl<T: Scalar> Add for Vector<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.0.len() != rhs.0.len() {
+            panic!("  dont have  same length");
+        }
+        let mut result = Vec::new();
+        for i in 0..self.0.len() {
+            result.push(self.0[i] + rhs.0[i]);
+        }
+        Vector(result)
+    }
+}
+
+impl<T: Scalar> Vector<T> {
+    pub fn new() -> Self {
+        Vector(Vec::new())
+    }
+
+    pub fn dot(&self, other: &Self) -> Option<T> {
+        if self.0.len() != other.0.len() {
+            return None;
+        }
+        let mut sum = T::zero();
+        for i in 0..self.0.len() {
+            sum = sum + (self.0[i] * other.0[i]);
+        }
+        Some(sum)
+    }
+}

2b22f8b11e39e22522f259495c328bd783584cd0
fahdaguenouz <faguenouz@gmail.com>
2025-08-16 12:43:00 +0100

    sales

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug,Default, Clone, PartialEq)]
 pub struct Store {
     pub products: Vec<(String, f32)>,
 }
@@ -10,10 +10,61 @@ impl Store {
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Cart {
-    // expected public fields
+    pub items:Vec<(String, f32)>,
+    pub receipt:Vec<f32>
 }
 impl Cart {
-    pub fn new() -> Cart {}
-    pub fn insert_item(&mut self, s: &Store, ele: String) {}
-    pub fn generate_receipt(&mut self) -> Vec<f32> {}
+    pub fn new() -> Cart {
+           Cart{
+            items:Vec::new(),
+            receipt:Vec::new(),
+           }
+        }
+    
+    pub fn insert_item(&mut self, s: &Store, ele: String) {
+            let mut price:f32=0.0;
+        for pr in &s.products{
+            if &ele==&pr.0{
+                price=pr.1;
+                break;
+            }
+            //  price =match ele{
+            //     pr.0=>pr.1,
+            //     _=>0.0,
+            // }
+            
+        }
+        self.items.push((ele,price));
+        self.generate_receipt();
+    }
+ pub fn get_prices(&self) -> Vec<f32> {
+        self.items.iter().map(|(_, p)| *p).collect()
+    }
+
+    pub fn generate_receipt(&mut self) -> Vec<f32> {
+        let mut prices = self.get_prices();
+        let cal = self.items.len() / 3; // number of "free items"
+        prices.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        // exclude cheapest "free" items
+        let v: Vec<f32> = prices[cal..].to_vec();
+
+        // compute scaling factor
+        let percentage: f32 = v.iter().sum::<f32>() * 100.0 / prices.iter().sum::<f32>();
+
+        // apply factor to all items
+        self.receipt = prices
+            .iter()
+            .map(|price| round_two(price * percentage / 100.0))
+            .collect();
+
+        self.receipt.clone()
+    }
 }
+
+fn round_two(nbr: f32) -> f32 {
+    (nbr * 100.0).round() / 100.0
+}
+
+
+// 1.23*((0.0+3.12 + 23.1)/(1.23, 3.12, 23.1))=1.17;

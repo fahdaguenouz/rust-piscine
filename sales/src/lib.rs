7a4c6b0d67321b0c62f697961572dd59484a3f39
fahdaguenouz <faguenouz@gmail.com>
2025-08-16 11:37:43 +0100

    sales

---
+++ sales/src/lib.rs
@@ -0,0 +1,19 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Store {
+    pub products: Vec<(String, f32)>,
+}
+impl Store {
+    pub fn new(products: Vec<(String, f32)>) -> Store {
+        Store { products }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Cart {
+    // expected public fields
+}
+impl Cart {
+    pub fn new() -> Cart {}
+    pub fn insert_item(&mut self, s: &Store, ele: String) {}
+    pub fn generate_receipt(&mut self) -> Vec<f32> {}
+}

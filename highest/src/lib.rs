bb1246610406d6effeb9d0d52c738d0b7c7af3bd
fahdaguenouz <faguenouz@gmail.com>
2025-08-16 16:14:28 +0100

    highest

--- highest/src/lib.rs
+++ highest/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug)]
+pub struct Numbers<'a> {
+    numbers: &'a [u32],
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<'a> Numbers<'a> {
+    pub fn new(numbers: &'a [u32]) -> Self {
+        Numbers { numbers }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn list(&self) -> &[u32] {
+        self.numbers
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.numbers.last().copied()
+        // let last = self.numbers.iter().rev();
+        // Some(last[0])
+    }
+
+    pub fn highest(&self) -> Option<u32> {
+         self.numbers.iter().max().copied()
+    }
+
+    pub fn highest_three(&self) -> Vec<u32> {
+         let mut nums = self.numbers.to_vec();
+        nums.sort_unstable_by(|a, b| b.cmp(a)); 
+        nums.into_iter().take(3).collect()
     }
 }

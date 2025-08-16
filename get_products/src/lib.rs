fadbbb719596557f8cc911093f4a8dbd3ca1150a
fahdaguenouz <faguenouz@gmail.com>
2025-08-16 15:42:36 +0100

    get products

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,14 +1,15 @@
-
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
-        let mut res = Vec::new();
-        for i in arr{
-            let sum =1; 
-            for j in arr{
-                if i==j{
-                    continue;
-                }else{
+    let mut res = Vec::new();
 
-                }
+    for i in 0..arr.len() {
+        let mut mul = 1;
+        for j in 0..arr.len() {
+            if i != j {
+                mul *= arr[j];
             }
         }
+        res.push(mul);
     }
+
+    res
+}
